06e7b340c4821a6574ddacd1a6426951b8e6e8df
anishkatta <56501640+Anish-katta@users.noreply.github.com>
2019-10-17 20:20:17 +0530

    Update Q1.py

--- Q1.py
+++ Q1.py
@@ -2,7 +2,6 @@ def generator(s,c,g):
 	for i in range(c):
 		print(s,end=" ")
 		s=s*g
-	print(s)
 a=int(input("Enter the starting number:"))
 b=int(input("Enter the number of terms to be generated:"))
 c=int(input("Enter the multipler:"))
